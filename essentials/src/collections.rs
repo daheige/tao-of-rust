58d1222f24e404d52038f659eba5183f55e06ad9
denglitong <litongdeng@gmail.com>
2019-10-31 08:31:07 +0800

    collections: HashMap, BTreeMap, HashSet, BTreeSet, BinaryHeap(max-heap)
    Signed-off-by: denglitong <litongdeng@gmail.com>

--- essentials/src/collections.rs
+++ essentials/src/collections.rs
@@ -5,7 +5,9 @@
 
 #[cfg(test)]
 mod tests {
-    use std::collections::{LinkedList, VecDeque};
+    use std::collections::{
+        BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque,
+    };
 
     #[test]
     fn test_vec() {
@@ -54,5 +56,71 @@ mod tests {
         list1.push_front('e');
         list2.push_front('f');
         assert_eq!(list1.pop_back(), Some('c'));
+    }
+
+    #[test]
+    fn test_kv() {
+        // HashMap/BTreeMap<K: Hash, V: Sized> K需要是可哈希的，V需要是在编译器已知大小的
+        let mut hmap = HashMap::new();
+        hmap.insert(3, "c");
+        hmap.insert(1, "a");
+        hmap.insert(2, "b");
+        hmap.insert(5, "e");
+        hmap.insert(4, "d");
+        println!("{:?}", hmap);
+
+        let mut bmap = BTreeMap::new();
+        bmap.insert(3, "c");
+        bmap.insert(2, "b");
+        bmap.insert(1, "a");
+        bmap.insert(5, "e");
+        bmap.insert(4, "d");
+        println!("{:?}", bmap);
+    }
+
+    #[test]
+    fn test_set() {
+        // HashSet<K>/BTreeSet<K> 其实就是 HashMap<K,V>和BTreeMap<K,V>把V设置为空元祖的特定类型
+        // HashSet<K> => HashSet<K,()>
+        let mut hbooks = HashSet::new();
+        hbooks.insert("A song of Ice and Fire");
+        hbooks.insert("The Emerald City");
+        hbooks.insert("The Odyssey");
+        if !hbooks.contains("The Emerald City") {
+            println!(
+                "We have {} bookes, but The Emerald City ain't one.",
+                hbooks.len()
+            );
+        }
+        println!("{:?}", hbooks);
+
+        let mut bbooks = BTreeSet::new();
+        bbooks.insert("A song of Ice and Fire");
+        bbooks.insert("The Emerald City");
+        bbooks.insert("The Odyssey");
+        println!("{:?}", bbooks);
+    }
+
+    #[test]
+    fn test_binary_heap() {
+        // 优先队列：Rust提供的优先队列是基于二叉堆来实现的，
+        // Rust BinaryHeap will be a max-heap. if you want to have min-heap, change the Ord trait for T
+        // 二叉堆是完全二叉树或者是近似完全二叉树。二叉堆满足堆特性：
+        // 父节点的键值总是保持固定的序关系于任何一个子节点的键值，且每个节点的左子树和右子树都是一个二叉堆
+        let mut heap = BinaryHeap::new();
+        assert_eq!(heap.peek(), None);
+        let arr = [93, 80, 48, 53, 72, 30, 18, 36, 15, 35, 45];
+        for &i in arr.iter() {
+            heap.push(i);
+        }
+        assert_eq!(heap.peek(), Some(&93));
+        // [93, 80, 48, 53, 72, 30, 18, 36, 15, 35, 45]
+        println!("{:?}", heap);
+
+        while let Some(i) = heap.pop() {
+            print!("{} ", i);
+        }
+        // 93 80 72 53 48 45 36 35 30 18 15
+        println!();
     }
 }
